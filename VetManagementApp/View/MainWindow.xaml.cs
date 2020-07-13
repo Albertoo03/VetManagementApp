c05c2e4fc1de348c91804913ef35f186bd109055
Albertoo03 <albertoo13@wp.pl>
2020-07-13 23:45:28 +0200

    Import window xaml files from local project

--- VetManagementApp/View/MainWindow.xaml.cs
+++ VetManagementApp/View/MainWindow.xaml.cs
@@ -8,12 +8,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interactivity;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VetManagementApp
+namespace VetManagementApp.View
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
@@ -23,6 +24,28 @@ namespace VetManagementApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+    }
+
+
+    /// <summary>
+    /// Custom class implementing the TriggerAction which enables to set properties of associated objects on specified event trigger
+    /// </summary>
+    public class SetterAction : TriggerAction<DependencyObject>
+    {
+        public SetterAction()
+        {
+            Setters = new List<Setter>();
+        }
+
+        public List<Setter> Setters { get; set; }
+
+        protected override void Invoke(object parameter)
+        {
+            foreach (var item in Setters)
+            {
+                AssociatedObject.SetValue(item.Property, item.Value);
+            }
         }
     }
 }
