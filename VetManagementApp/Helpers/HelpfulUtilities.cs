703d6834b07ce0d8ec7c14d0ebd2425c25379914
Albertoo03 <albertoo13@wp.pl>
2020-07-13 23:33:28 +0200

    Method for checking if the window has already been opened

--- VetManagementApp/Helpers/HelpfulUtilities.cs
+++ VetManagementApp/Helpers/HelpfulUtilities.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace VetManagementApp.Helpers
 {
@@ -65,6 +66,21 @@ namespace VetManagementApp.Helpers
         {
             public float DoseInMiligrams { get; set; }
             //public en
+        }
+
+
+
+        /// <summary>
+        /// Checks if window of the name passed as an argument has not already been opened.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool IsWindowOpen<T>(string name = "") where T : Window
+        {
+            return string.IsNullOrEmpty(name)
+               ? Application.Current.Windows.OfType<T>().Any()
+               : Application.Current.Windows.OfType<T>().Any(w => w.Name.Equals(name));
         }
     }
 }
