6f4fba0bd1763770455d6c9ee412d9d97fe6c2f7
Albertoo03 <albertoo13@wp.pl>
2020-07-15 23:46:51 +0200

    Added validator for house number

---
+++ VetManagementApp/Validators/Validators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace VetManagementApp.Validators
+{
+    public class HouseNumberValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int houseNumber;
+            bool parsingSucceeded = Int32.TryParse(value.ToString(), out houseNumber);
+
+            if(!parsingSucceeded)
+                return new System.Windows.Controls.ValidationResult(false, "House number must be a number value.");
+
+            if (houseNumber < 1)
+                return new System.Windows.Controls.ValidationResult(false, "House number must be value greater than 0.");
+
+            return System.Windows.Controls.ValidationResult.ValidResult;
+        }
+    }
+}
